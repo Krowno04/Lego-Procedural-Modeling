bd286a6d34978dc09f808210af6e0bf92d219a71
Rayliable <rayrmrose@gmail.com>
2025-03-11 15:39:58 -0400

    Merged scripts

--- scripts/LegoModelScript_Explicit.py
+++ scripts/LegoModelScript_Explicit.py
@@ -10,6 +10,7 @@ cmds.menu(label="Basic Options")
 cmds.menuItem(label="New Scene", command=('cmds.file(new=True, force=True)'))
 cmds.menuItem(label="Delete Selected", command=('cmds.delete()'))
 
+
 cmds.frameLayout(collapsable=True, label="Standard Block", width=475, height=140)
 
 cmds.columnLayout()
@@ -26,6 +27,19 @@ cmds.setParent( '..' )
 
 cmds.setParent( '..' )
 
+cmds.frameLayout(collapsable=True, label="Smooth Block", width=475, height=140)
+
+cmds.columnLayout()
+cmds.intSliderGrp('smoothHeight',l="Height", f=True, min=1, max=20, value=1)
+cmds.intSliderGrp('smoothWidth', l="Width (No Bumps)", f=True, min=1, max=20, value=2)
+cmds.intSliderGrp('smoothDepth', l="Depth (No Bumps)", f=True, min=1, max=20, value=8)
+
+cmds.colorSliderGrp('smoothColour', label="Colour", hsv=(360, 1, 1))
+cmds.columnLayout()
+cmds.button(label="Create Smooth Block", command=('smoothBlock()'))
+cmds.setParent( '..' )
+cmds.setParent( '..' )
+cmds.setParent( '..' )
 
 cmds.frameLayout(collapsable=True, label="L-Shaped Block", width=475, height=140)
 
@@ -128,6 +142,59 @@ def slopedBlock():
         cmds.move(tV[0], tV[1], tV[2], a=True)
 
     cmds.namespace( removeNamespace=":"+nsTmp, mergeNamespaceWithParent = True)
+
+    
+def smoothBlock():
+    blockHeight = cmds.intSliderGrp('smoothHeight', q=True, v=True)
+    blockWidth = cmds.intSliderGrp('smoothWidth', q=True, v=True)
+    blockDepth = cmds.intSliderGrp('smoothDepth', q=True, v=True)
+    
+    rgb = cmds.colorSliderGrp('smoothColour', q=True, rgbValue=True)
+    nsTmp = "Block" + str(rnd.randint(1000,9999))
+    
+    cmds.select(clear=True)
+    cmds.namespace(add=nsTmp)
+    cmds.namespace(set=nsTmp)
+    
+    cubeSizeX = blockWidth * 0.8
+    cubeSizeZ = blockDepth * 0.8
+    cubeSizeY = blockHeight * 0.32
+    
+    print(cubeSizeX, "\n", cubeSizeY, "\n", cubeSizeZ, "\n", rgb, "\n", nsTmp, "\n\n\n")
+
+    cmds.polyCube(h=cubeSizeY, w=cubeSizeX, d=cubeSizeZ)
+
+    print(cubeSizeX, "\n", cubeSizeY, "\n", cubeSizeZ, "\n", rgb, "\n", nsTmp, "\n\n\n")
+
+    cmds.move((cubeSizeY/2.0), moveY=True)
+
+    print(cubeSizeX, "\n", cubeSizeY, "\n", cubeSizeZ, "\n", rgb, "\n", nsTmp, "\n\n\n")
+
+    # This commented part is the temporary solution of moving a single, tiny stud so it can be united
+    # cmds.polyCylinder(r=0.01, h=0.01)
+    # cmds.move((cubeSizeY - 0.1), moveY=True, a=True)
+    # cmds.move(((0.1 * 0.8) - (cubeSizeX/2.0) + 0.4), moveX=True, a=True)
+    # cmds.move(((0.1 * 0.8) - (cubeSizeZ/2.0) + 0.4), moveZ=True, a=True)
+    
+
+    
+    myShader = cmds.shadingNode('lambert', asShader=True, name="blckMat")
+    cmds.setAttr(nsTmp+":blckMat.color",rgb[0],rgb[1],rgb[2], typ='double3')
+
+    print(cubeSizeX, "\n", cubeSizeY, "\n", cubeSizeZ, "\n", rgb, "\n", nsTmp, "\n\n\n")
+
+    # The rename from Polyunite
+    # cmds.polyUnite((nsTmp+":*"), n=nsTmp, ch=False)
+    # cmds.delete(ch=True)
+    cmds.select(nsTmp+':pCube1', r=True)
+    cmds.rename(nsTmp)
+    try:
+        cmds.hyperShade(assign=(nsTmp+":blckMat"))
+    except:
+        pass
+    cmds.namespace(removeNamespace=":"+nsTmp,mergeNamespaceWithParent=True)
+    print('here')
+
 
 
 def roundBlock():
